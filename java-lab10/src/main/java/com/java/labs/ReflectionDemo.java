b2d5e8225b3af02f66122ef8d026ce0d6d885e67
ivan_yakovchuk <yakovchuk1005@gmail.com>
2025-11-15 11:32:02 +0200

    Lab 10 completed

---
+++ java-lab10/src/main/java/com/java/labs/ReflectionDemo.java
@@ -0,0 +1,26 @@
+package com.java.labs;
+
+import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
+
+public class ReflectionDemo {
+    static void main() throws IllegalAccessException, NoSuchFieldException {
+        String string = "string";
+//        String myString = IO.readln("Enter the string: ");
+
+        System.out.println("Before: " + string);
+
+        Field valueField = String.class.getDeclaredField("value");
+        valueField.setAccessible(true);
+
+        byte[] value = (byte[]) valueField.get(string);
+
+        String newString = "new string";
+//        String myNewString = IO.readln("Enter new string: ");
+
+        byte[] bytes = newString.getBytes(StandardCharsets.UTF_8);
+        valueField.set(string, bytes);
+
+        System.out.println("After: " + string);
+    }
+}
